e1e4d1256a696f60e4e9b6596d024a95657da159
Pierre Axel Izerable <pierreaxel.iz@gmail.com>
2024-07-02 00:05:32 +0200

    code OK mais petit but sur le scode

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
+  root to: 'games#new'
   get 'new', to: 'games#new'
   post 'score', to: 'games#score'
-  root to: 'games#new'
 
   # Defines the root path route ("/")
   # root "posts#index"
